199b405ce9f558f0c0f40f51ac68e04497080d0e
vidvidvid <weetopol@gmail.com>
2022-04-08 19:02:06 +0200

    feat: set up search input for DAOs

--- packages/dapp/pages/search.tsx
+++ packages/dapp/pages/search.tsx
@@ -1,9 +1,58 @@
-import { Heading, VStack } from '@chakra-ui/react';
+/* eslint-disable no-console */
+import { Box, Heading, Input, VStack } from '@chakra-ui/react';
+import { SetStateAction, useEffect, useState } from 'react';
+
+const dummyDAOs = [
+  {
+    name: 'MetaGame',
+  },
+  {
+    name: 'MetaCartel',
+  },
+  {
+    name: 'KlimaDAO',
+  },
+  {
+    name: 'OlympusDAO',
+  },
+  {
+    name: '1Hive',
+  },
+  {
+    name: 'Giveth',
+  },
+];
+
+interface DAOsData {
+  name: string;
+}
 
 const Search: React.FC = () => {
+  const [value, setValue] = useState('');
+  const handleChange = (event: { target: { value: SetStateAction<string> } }) =>
+    setValue(event.target.value);
+
+  const [results, setResults] = useState<DAOsData[]>([]);
+
+  useEffect(() => {
+    console.log('new value of input: ', value);
+
+    const filteredDAOs = dummyDAOs.filter(DAO => DAO.name.includes(value));
+
+    setResults(filteredDAOs);
+  }, [value]);
+
   return (
     <VStack>
       <Heading>Search for DAO</Heading>
+      <Input
+        placeholder="Search for DAO"
+        value={value}
+        onChange={handleChange}
+      />
+      {results.map(({ name }) => (
+        <Box key={name}>{name}</Box>
+      ))}
     </VStack>
   );
 };
