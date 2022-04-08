94745ad53891fff23f5c405b92a50e088a65984c
vidvidvid <weetopol@gmail.com>
2022-04-08 23:47:36 +0200

    style: add funky background

--- packages/dapp/pages/_app.tsx
+++ packages/dapp/pages/_app.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-unresolved */
 import 'focus-visible/dist/focus-visible';
+import '@/assets/bg.scss';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
@@ -10,6 +12,15 @@ import { globalStyles, theme } from '@/utils/theme';
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <Global styles={globalStyles} />
       <AppLayout>
         <Component {...pageProps} />
