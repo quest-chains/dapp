c86bf939dac82b8a30347ffc0a25a9ddb6d33767
dan13ram <dan13ram@gmail.com>
2022-04-09 23:27:55 +0530

    fixed connect button

--- packages/dapp/components/AppLayout.tsx
+++ packages/dapp/components/AppLayout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import {
   Box,
+  Flex,
   Heading,
   HStack,
   Link as ChakraLink,
@@ -30,59 +31,65 @@ export const AppLayout: React.FC<{ children: JSX.Element }> = ({
         w="100%"
         mx="auto"
       >
-        <HStack w="100%" justify="space-between" pos="relative">
-          <NextLink href="/" passHref>
-            <ChakraLink display="block" _hover={{}} w="20%">
-              {router.pathname !== '/' && (
-                <Heading color="main" fontSize={30} fontWeight="bold">
-                  DAOQuest
-                </Heading>
-              )}
-            </ChakraLink>
-          </NextLink>
-          {router.pathname !== '/' && (
-            <HStack gap={4}>
-              <NextLink href="/search" passHref>
-                <ChakraLink display="block" _hover={{}}>
-                  <Text
-                    borderBottomWidth={router.pathname === '/search' ? 1 : 0}
-                    borderBottomColor="main"
-                    color={router.pathname === '/search' ? 'main' : 'inherit'}
-                  >
-                    SEARCH FOR QUEST CHAIN
-                  </Text>
-                </ChakraLink>
-              </NextLink>
-              <NextLink href="/create" passHref>
-                <ChakraLink display="block" _hover={{}}>
-                  <Text
-                    borderBottomWidth={router.pathname === '/create' ? 1 : 0}
-                    borderBottomColor="main"
-                    color={router.pathname === '/create' ? 'main' : 'inherit'}
-                  >
-                    CREATE QUEST CHAIN
-                  </Text>
-                </ChakraLink>
-              </NextLink>
-              <NextLink href="/overview" passHref>
-                <ChakraLink display="block" _hover={{}}>
-                  <Text
-                    borderBottomWidth={router.pathname === '/overview' ? 1 : 0}
-                    borderBottomColor="main"
-                    color={router.pathname === '/overview' ? 'main' : 'inherit'}
-                  >
-                    QUESTS OVERVIEW
-                  </Text>
-                </ChakraLink>
-              </NextLink>
-            </HStack>
-          )}
+        {isConnected && (
+          <HStack w="100%" justify="space-between" pos="relative">
+            <NextLink href="/" passHref>
+              <ChakraLink display="block" _hover={{}} w="20%">
+                {router.pathname !== '/' && (
+                  <Heading color="main" fontSize={30} fontWeight="bold">
+                    DAOQuest
+                  </Heading>
+                )}
+              </ChakraLink>
+            </NextLink>
+            {router.pathname !== '/' && (
+              <HStack gap={4}>
+                <NextLink href="/search" passHref>
+                  <ChakraLink display="block" _hover={{}}>
+                    <Text
+                      borderBottomWidth={router.pathname === '/search' ? 1 : 0}
+                      borderBottomColor="main"
+                      color={router.pathname === '/search' ? 'main' : 'inherit'}
+                    >
+                      SEARCH FOR QUEST CHAIN
+                    </Text>
+                  </ChakraLink>
+                </NextLink>
+                <NextLink href="/create" passHref>
+                  <ChakraLink display="block" _hover={{}}>
+                    <Text
+                      borderBottomWidth={router.pathname === '/create' ? 1 : 0}
+                      borderBottomColor="main"
+                      color={router.pathname === '/create' ? 'main' : 'inherit'}
+                    >
+                      CREATE QUEST CHAIN
+                    </Text>
+                  </ChakraLink>
+                </NextLink>
+                <NextLink href="/overview" passHref>
+                  <ChakraLink display="block" _hover={{}}>
+                    <Text
+                      borderBottomWidth={
+                        router.pathname === '/overview' ? 1 : 0
+                      }
+                      borderBottomColor="main"
+                      color={
+                        router.pathname === '/overview' ? 'main' : 'inherit'
+                      }
+                    >
+                      QUESTS OVERVIEW
+                    </Text>
+                  </ChakraLink>
+                </NextLink>
+              </HStack>
+            )}
 
-          <Box w="20%" textAlign="right">
-            {isConnected && <ConnectWallet isHeader />}
-          </Box>
-          {/* <NavToggle isOpen={isOpen} onClick={toggleOpen} /> */}
-        </HStack>
+            <Box w="20%" textAlign="right">
+              <ConnectWallet isHeader />
+            </Box>
+            {/* <NavToggle isOpen={isOpen} onClick={toggleOpen} /> */}
+          </HStack>
+        )}
       </VStack>
       <Box
         w="100%"
@@ -91,9 +98,23 @@ export const AppLayout: React.FC<{ children: JSX.Element }> = ({
         px={8}
         maxW="8xl"
         mx="auto"
-        overflow="scroll"
+        overflowX="hidden"
       >
-        {children}
+        {isConnected ? (
+          children
+        ) : (
+          <Flex
+            h="full"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Heading color="main" fontSize={87} pb={10}>
+              DAOQuest
+            </Heading>
+            <ConnectWallet />
+          </Flex>
+        )}
       </Box>
     </Stack>
   );
