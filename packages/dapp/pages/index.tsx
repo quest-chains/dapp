ee5704bd9c69a4ef1dcc969fb24643398729430c
vidvidvid <weetopol@gmail.com>
2022-04-09 12:25:15 +0200

    style landing page

--- packages/dapp/pages/index.tsx
+++ packages/dapp/pages/index.tsx
@@ -1,7 +1,18 @@
-import { Flex, Heading, Link as ChakraLink } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link as ChakraLink,
+  VStack,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [isEntered, setIsEntered] = useState(false);
+
   return (
     <Flex
       h="full"
@@ -9,24 +20,101 @@ const Home: React.FC = () => {
       alignItems="center"
       flexDirection="column"
     >
-      <Heading color="main" fontSize={87}>
+      <Heading color="main" fontSize={87} pb={10}>
         DAOQuest
       </Heading>
-      <NextLink href="/search" passHref>
-        <ChakraLink display="block" _hover={{}}>
-          Search For DAO
-        </ChakraLink>
-      </NextLink>
-      <NextLink href="/create" passHref>
-        <ChakraLink display="block" _hover={{}}>
-          Create Quest Chain
-        </ChakraLink>
-      </NextLink>
-      <NextLink href="/overview" passHref>
-        <ChakraLink display="block" _hover={{}}>
-          Quests overview
-        </ChakraLink>
-      </NextLink>
+
+      {!isConnected && (
+        <Button
+          px={20}
+          background="rgba(255, 255, 255, 0.05)"
+          fontWeight="400"
+          backdropFilter="blur(40px)"
+          borderRadius="full"
+          boxShadow="inset 0px 0px 0px 1px #AD90FF"
+          onClick={() => setIsConnected(true)}
+          color="main"
+          letterSpacing={4}
+          fontSize={30}
+          height={16}
+        >
+          connect
+        </Button>
+      )}
+
+      {!isEntered && isConnected && (
+        <Button
+          px={20}
+          background="rgba(255, 255, 255, 0.05)"
+          fontWeight="400"
+          backdropFilter="blur(40px)"
+          borderRadius="full"
+          boxShadow="inset 0px 0px 0px 1px #AD90FF"
+          onClick={() => setIsEntered(true)}
+          color="main"
+          letterSpacing={4}
+          fontSize={30}
+          height={16}
+        >
+          enter
+        </Button>
+      )}
+
+      {isEntered && (
+        <VStack spacing={6}>
+          <NextLink href="/search" passHref>
+            <Button
+              px={20}
+              background="rgba(255, 255, 255, 0.05)"
+              fontWeight="400"
+              backdropFilter="blur(40px)"
+              borderRadius="full"
+              boxShadow="inset 0px 0px 0px 1px #AD90FF"
+              onClick={() => setIsEntered(true)}
+              color="main"
+              letterSpacing={4}
+              fontSize={30}
+              height={16}
+            >
+              search For DAO
+            </Button>
+          </NextLink>
+          <NextLink href="/create" passHref>
+            <Button
+              px={20}
+              background="rgba(255, 255, 255, 0.05)"
+              fontWeight="400"
+              backdropFilter="blur(40px)"
+              borderRadius="full"
+              boxShadow="inset 0px 0px 0px 1px #AD90FF"
+              onClick={() => setIsEntered(true)}
+              color="main"
+              letterSpacing={4}
+              fontSize={30}
+              height={16}
+            >
+              create Quest Chain
+            </Button>
+          </NextLink>
+          <NextLink href="/overview" passHref>
+            <Button
+              px={20}
+              background="rgba(255, 255, 255, 0.05)"
+              fontWeight="400"
+              backdropFilter="blur(40px)"
+              borderRadius="full"
+              boxShadow="inset 0px 0px 0px 1px #AD90FF"
+              onClick={() => setIsEntered(true)}
+              color="main"
+              letterSpacing={4}
+              fontSize={30}
+              height={16}
+            >
+              quests overview
+            </Button>
+          </NextLink>
+        </VStack>
+      )}
     </Flex>
   );
 };
