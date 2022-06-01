a1ec2a0a3ef67a4f5dc4370e6aee98569c91d0a8
dan13ram <dan13ram@gmail.com>
2022-06-01 12:29:54 +0530

    fixed profile page logic

--- packages/dapp/pages/profile/[address].tsx
+++ packages/dapp/pages/profile/[address].tsx
@@ -1,57 +1,55 @@
 import { Box, Grid, Text, VStack } from '@chakra-ui/react';
+import { utils } from 'ethers';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { QuestsRejected } from '@/components/QuestsRejected';
 import { QuestsToReview } from '@/components/QuestsToReview';
 import { UserProgress } from '@/components/UserProgress';
 import { UserRoles } from '@/components/UserRoles';
-import { useWallet } from '@/web3';
+import { formatAddress, useWallet } from '@/web3';
 
 type Props = { address: string };
 
-const Explore: React.FC<Props> = () => {
-  const [showChild, setShowChild] = useState(false);
+const Profile: React.FC<Props> = ({ address: addressURL }) => {
   const { address } = useWallet();
+  const isLoggedInUser = addressURL === address;
 
+  const { push } = useRouter();
+  const isValidAddress = utils.isAddress(addressURL);
   useEffect(() => {
-    setShowChild(true);
-  }, []);
+    if (!isValidAddress) {
+      push('/');
+    }
+  }, [isValidAddress, push]);
 
-  if (!showChild) {
+  if (!isValidAddress) {
     return null;
   }
 
-  if (typeof window !== 'undefined') {
-    const addressURL = window.location.href.split('/').pop() || '';
-    const isLoggedInUser = addressURL === address;
+  return (
+    <VStack px={{ base: 0, lg: 40 }} alignItems="flex-start" gap={4}>
+      <Head>
+        <title>Quest Chains</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <Box>
+          <Text w="100%" textAlign="left" mb={2} color="main" fontSize={20}>
+            USER
+          </Text>
+          <Text>{formatAddress(addressURL)}</Text>
+        </Box>
 
-    return (
-      <VStack px={{ base: 0, lg: 40 }} alignItems="flex-start" gap={4}>
-        <Head>
-          <title>Quest Chains</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-          <Box>
-            <Text w="100%" textAlign="left" mb={2} color="main" fontSize={20}>
-              USER
-            </Text>
-            <Text>{addressURL}</Text>
-          </Box>
+        <UserRoles address={addressURL} />
+        <UserProgress address={addressURL} />
 
-          <UserRoles address={addressURL} />
-          <UserProgress address={addressURL} />
-
-          {isLoggedInUser && <QuestsToReview address={addressURL} />}
-          {isLoggedInUser && <QuestsRejected address={addressURL} />}
-        </Grid>
-      </VStack>
-    );
-  } else return <></>;
+        {isLoggedInUser && <QuestsToReview address={addressURL} />}
+        {isLoggedInUser && <QuestsRejected address={addressURL} />}
+      </Grid>
+    </VStack>
+  );
 };
 
-export default Explore;
+export default Profile;
