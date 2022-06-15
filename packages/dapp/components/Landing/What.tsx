b5096f2d4c5a3a1da4fc8c02e2542dab286f7eca
vidvidvid <weetopol@gmail.com>
2022-06-15 13:49:07 +0200

    remove animations and move 2nd slide more up

--- packages/dapp/components/Landing/What.tsx
+++ packages/dapp/components/Landing/What.tsx
@@ -1,27 +1,29 @@
-import { Flex, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import { Flex, Heading, Image, Text } from '@chakra-ui/react';
 import { useRef } from 'react';
-
-import { useOnScreen } from '@/hooks/useOnScreen';
 
 export const What: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const onScreen = useOnScreen(ref);
 
   return (
-    <HStack w="full" align="center" justify="center" minH="70vh" id="what">
+    <Flex
+      w="full"
+      align="start"
+      justify="center"
+      minH="40vh"
+      id="what"
+      gap={10}
+      pt={20}
+    >
       <Image src="Landing/Circles.svg" alt="circles" />
       <Flex
         ref={ref}
         display="flex"
         flexDirection="column"
-        justifyContent="center"
+        justify="baseline"
         maxWidth={{ base: '90%', md: '5xl' }}
         lineHeight={{ base: 'lg', '2xl': '2xl' }}
         pl={{ base: 0, md: 0 }}
         zIndex={100}
-        transform={`translate3d(0, ${onScreen ? '0' : '50px'}, 0)`}
-        opacity={onScreen ? 1 : 0}
-        transition="transform 0.1s 0.1s ease-in-out, opacity 0.1s 0.1s ease-in"
         fontWeight="normal"
         color="white"
       >
@@ -44,6 +46,6 @@ export const What: React.FC = () => {
           NFTs, which would showcase their newly acquired skills in profiles.
         </Text>
       </Flex>
-    </HStack>
+    </Flex>
   );
 };
