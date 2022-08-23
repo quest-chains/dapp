227df8885f29fdaa8bf2d8d13e83fb2f408a5fb3
vidvidvid <weetopol@gmail.com>
2022-08-23 14:27:47 +0200

    add modal with premium chain info

--- components/CreateChain/NFTForm.tsx
+++ components/CreateChain/NFTForm.tsx
@@ -1,4 +1,21 @@
-import { Box, Button, Flex, HStack, Text, VStack } from '@chakra-ui/react';
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 
 import NFTForm2D from '@/components/CreateChain/NFTForm2D';
@@ -10,7 +27,17 @@ const NFTForm: React.FC<{
   onSubmit: (metadataUri: string, nftUrl?: string) => void | Promise<void>;
   chainName: string;
 }> = ({ onSubmit, chainName }) => {
-  const [selectedNFT, setSelectedNFT] = useState('2D'); // 3D, custom
+  const [tab, setTab] = useState('2D'); // 3D, custom
+  const [didOpenPremiumTab, setDidOpenPremiumTab] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const switchTab = (tab: string) => {
+    setTab(tab);
+    if (didOpenPremiumTab) return;
+
+    onOpen();
+    setDidOpenPremiumTab(true);
+  };
 
   return (
     <VStack
@@ -22,22 +49,31 @@ const NFTForm: React.FC<{
       px={{ base: 4, md: 12 }}
       py={8}
     >
-      <HStack w="100%">
-        <Box
-          py={1}
-          px={3}
-          borderWidth={1}
-          borderColor="gray.500"
-          color="gray.500"
-          borderRadius={4}
-          mr={4}
-        >
-          STEP 2
-        </Box>
-        <Text fontWeight="bold" fontSize={16}>
-          Chain completion NFT
-        </Text>
-      </HStack>
+      <Flex w="100%" justifyContent="space-between" alignItems="center">
+        <Flex alignItems="center">
+          <Box
+            py={1}
+            px={3}
+            borderWidth={1}
+            borderColor="gray.500"
+            color="gray.500"
+            borderRadius={4}
+            mr={4}
+          >
+            STEP 2
+          </Box>
+          <Text fontWeight="bold" fontSize={16}>
+            Chain completion NFT
+          </Text>
+        </Flex>
+
+        <IconButton
+          icon={<QuestionOutlineIcon />}
+          onClick={onOpen}
+          bg="transparent"
+          aria-label="info"
+        />
+      </Flex>
 
       <Flex
         justifyContent="space-between"
@@ -45,22 +81,22 @@ const NFTForm: React.FC<{
         borderColor="white"
       >
         <Button
-          onClick={() => setSelectedNFT('2D')}
+          onClick={() => switchTab('2D')}
           py={2}
           flexGrow={1}
           borderBottomWidth={2}
-          borderBottomColor={selectedNFT === '2D' ? 'white' : 'transparent'}
+          borderBottomColor={tab === '2D' ? 'white' : 'transparent'}
           borderRadius={0}
           variant="ghost"
         >
           2D NFT
         </Button>
         <Button
-          onClick={() => setSelectedNFT('3D')}
+          onClick={() => switchTab('3D')}
           py={2}
           flexGrow={1}
           borderBottomWidth={2}
-          borderBottomColor={selectedNFT === '3D' ? 'white' : 'transparent'}
+          borderBottomColor={tab === '3D' ? 'white' : 'transparent'}
           borderRadius={0}
           variant="ghost"
         >
@@ -79,11 +115,11 @@ const NFTForm: React.FC<{
           </Box>
         </Button>
         <Button
-          onClick={() => setSelectedNFT('custom')}
+          onClick={() => switchTab('custom')}
           py={2}
           flexGrow={1}
           borderBottomWidth={2}
-          borderBottomColor={selectedNFT === 'custom' ? 'white' : 'transparent'}
+          borderBottomColor={tab === 'custom' ? 'white' : 'transparent'}
           borderRadius={0}
           variant="ghost"
         >
@@ -102,15 +138,41 @@ const NFTForm: React.FC<{
           </Box>
         </Button>
       </Flex>
-      {selectedNFT === '2D' && (
-        <NFTForm2D chainName={chainName} onSubmit={onSubmit} />
-      )}
-      {selectedNFT === '3D' && (
-        <NFTForm3D chainName={chainName} onSubmit={onSubmit} />
-      )}
-      {selectedNFT === 'custom' && (
+      {tab === '2D' && <NFTForm2D chainName={chainName} onSubmit={onSubmit} />}
+      {tab === '3D' && <NFTForm3D chainName={chainName} onSubmit={onSubmit} />}
+      {tab === 'custom' && (
         <NFTFormCustom chainName={chainName} onSubmit={onSubmit} />
       )}
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent maxW="40rem">
+          <ModalHeader>About 3D NFTs</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Are you ready to take your quest chain to the next level? Make it
+            stand out with this amazing 3D NFT badge or a custom image that can
+            be added to your chain’s overview page, and will look equally as
+            amazing on the chain’s questers’ profiles. This premium feature
+            costs 2 SEED tokens (~10$), which are MetaGame’s native tokens. Read
+            how to acquire them
+            <Link
+              mx={1}
+              isExternal
+              href="https://metagame.wtf/seeds"
+              textDecoration="underline"
+            >
+              here.
+            </Link>
+          </ModalBody>
+
+          <ModalFooter alignItems="baseline">
+            <Button variant="ghost" mr={3} onClick={onClose}>
+              Gotcha, close this message
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </VStack>
   );
 };
