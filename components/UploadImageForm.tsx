a262d0eac45b1c1ba95b6c658ed723fae79a4a1c
dan13ram <dan13ram@gmail.com>
2022-12-14 20:02:01 +0530

    support editing of cover image

--- components/UploadImageForm.tsx
+++ components/UploadImageForm.tsx
@@ -9,6 +9,7 @@ import {
   Image,
 } from '@chakra-ui/react';
 import { ImageProps } from 'next/image';
+import { useState } from 'react';
 
 import { DropImageType } from '@/hooks/useDropFiles';
 
@@ -22,56 +23,93 @@ export const UploadImageForm = ({
   labelColor = 'white',
   formControlProps = {},
   imageProps = {},
+  defaultImageUri: defaultImageUriInput,
+  onResetDefaultImage,
 }: DropImageType & {
+  defaultImageUri?: string | null | undefined;
+  onResetDefaultImage?: () => void;
   label?: string;
   labelColor?: string;
   formControlProps?: FormControlProps;
   imageProps?: Omit<ImageProps, 'src'> & BoxProps;
-}) => (
-  <FormControl {...formControlProps}>
-    <FormLabel color={labelColor} htmlFor="imageInput">
-      {label}
-    </FormLabel>
-    {imageFile ? (
-      <Flex key={imageFile.name} pos="relative">
-        {typeof window !== 'undefined' && (
-          <Image
-            {...imageProps}
-            alt={`${label} imageFile`}
-            src={window.URL.createObjectURL(imageFile)}
+}) => {
+  const [defaultImageUri, setDefaultImageUri] = useState<string>(
+    defaultImageUriInput ?? '',
+  );
+
+  return (
+    <FormControl {...formControlProps}>
+      <FormLabel color={labelColor} htmlFor="imageInput">
+        {label}
+      </FormLabel>
+      {imageFile && (
+        <Flex pos="relative">
+          {typeof window !== 'undefined' && (
+            <Image
+              {...imageProps}
+              alt={`${label} imageFile`}
+              src={window.URL.createObjectURL(imageFile)}
+            />
+          )}
+          <IconButton
+            pos="absolute"
+            size="sm"
+            top={2}
+            left={2}
+            borderRadius="full"
+            onClick={onResetImage}
+            icon={<SmallCloseIcon boxSize="1.5rem" />}
+            aria-label={''}
+            backdropFilter="blur(40px)"
+            boxShadow="inset 0px 0px 0px 1px white"
           />
-        )}
-        <IconButton
-          pos="absolute"
-          size="sm"
-          top={2}
-          left={2}
-          borderRadius="full"
-          onClick={onResetImage}
-          icon={<SmallCloseIcon boxSize="1.5rem" />}
-          aria-label={''}
-          backdropFilter="blur(40px)"
-          boxShadow="inset 0px 0px 0px 1px white"
-        />
-      </Flex>
-    ) : (
-      <Flex
-        {...dropzoneProps}
-        flexDir="column"
-        borderWidth={1}
-        borderStyle="dashed"
-        borderRadius={20}
-        p={10}
-        onClick={onOpenImageInput}
-      >
-        <input {...inputProps} id="imageInput" color="white" />
+        </Flex>
+      )}
+      {!imageFile && defaultImageUri && onResetDefaultImage && (
+        <Flex pos="relative">
+          {typeof window !== 'undefined' && (
+            <Image
+              {...imageProps}
+              alt={`${label} imageFile`}
+              src={defaultImageUri}
+            />
+          )}
+          <IconButton
+            pos="absolute"
+            size="sm"
+            top={2}
+            left={2}
+            borderRadius="full"
+            onClick={() => {
+              onResetDefaultImage();
+              setDefaultImageUri('');
+            }}
+            icon={<SmallCloseIcon boxSize="1.5rem" />}
+            aria-label={''}
+            backdropFilter="blur(40px)"
+            boxShadow="inset 0px 0px 0px 1px white"
+          />
+        </Flex>
+      )}
+      {!imageFile && (!defaultImageUri || !onResetDefaultImage) && (
         <Flex
-          justifyContent="center"
-          alignItems="center"
-          alignSelf="center"
-          height="100%"
-        >{`Drag 'n' drop an image here`}</Flex>
-      </Flex>
-    )}
-  </FormControl>
-);
+          {...dropzoneProps}
+          flexDir="column"
+          borderWidth={1}
+          borderStyle="dashed"
+          borderRadius={20}
+          p={10}
+          onClick={onOpenImageInput}
+        >
+          <input {...inputProps} id="imageInput" color="white" />
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            alignSelf="center"
+            height="100%"
+          >{`Drag 'n' drop an image here`}</Flex>
+        </Flex>
+      )}
+    </FormControl>
+  );
+};
