ce9313c7a04a73520470200c26fb44aff9db2588
vidvidvid <weetopol@gmail.com>
2022-12-22 14:32:43 +0100

    creating + editing

--- web3/networks.ts
+++ web3/networks.ts
@@ -8,6 +8,7 @@ export type NetworkInfo = {
     chainId: string;
     name: string;
     label: string;
+    urlName: string;
     symbol: string;
     explorer: string;
     explorerLabel: string;
@@ -18,72 +19,85 @@ export type NetworkInfo = {
   };
 };
 
+export const CHAIN_URL_MAPPINGS: { [chainId: string]: string } = {
+  ethereum: '0x1',
+  polygon: '0x89',
+  xdai: '0x64',
+  goerli: '0x5',
+  mumbai: '0x13881',
+};
+
 export const AVAILABLE_NETWORK_INFO: NetworkInfo = {
   '0x1': {
     chainId: '0x1',
     name: 'Ethereum Mainnet',
     label: 'Ethereum',
+    urlName: CHAIN_URL_MAPPINGS.ethereum,
     symbol: 'ETH',
     explorer: 'https://etherscan.io',
     explorerLabel: 'Etherscan',
     rpc: `https://mainnet.infura.io/v3/${INFURA_ID}`,
     image: EthereumImage.src,
     // TODO: deploy contract & subgraph
-    subgraphName: 'dan13ram/quest-chains-mainnet',
+    subgraphName: 'quest-chains/quest-chains-mainnet',
     subgraphUrl:
-      'https://api.thegraph.com/subgraphs/name/dan13ram/quest-chains-mainnet',
+      'https://api.thegraph.com/subgraphs/name/quest-chains/quest-chains-mainnet',
   },
   '0x89': {
     chainId: '0x89',
     name: 'Polygon Mainnet',
     label: 'Polygon',
+    urlName: CHAIN_URL_MAPPINGS.polygon,
     symbol: 'MATIC',
     explorer: 'https://polygonscan.com',
     explorerLabel: 'PolygonScan',
     rpc: `https://polygon-rpc.com`,
     image: PolygonImage.src,
-    subgraphName: 'dan13ram/quest-chains-polygon',
+    subgraphName: 'quest-chains/quest-chains-polygon',
     subgraphUrl:
-      'https://api.thegraph.com/subgraphs/name/dan13ram/quest-chains-polygon',
+      'https://api.thegraph.com/subgraphs/name/quest-chains/quest-chains-polygon',
   },
   '0x64': {
     chainId: '0x64',
     name: 'Gnosis Chain',
     label: 'Gnosis',
+    urlName: CHAIN_URL_MAPPINGS.xdai,
     symbol: 'xDAI',
     explorer: 'https://blockscout.com/xdai/mainnet',
     explorerLabel: 'Blockscout',
     rpc: 'https://rpc.gnosischain.com/',
     image: GnosisImage.src,
-    subgraphName: 'dan13ram/quest-chains-xdai',
+    subgraphName: 'quest-chains/quest-chains-xdai',
     subgraphUrl:
-      'https://api.thegraph.com/subgraphs/name/dan13ram/quest-chains-xdai',
+      'https://api.thegraph.com/subgraphs/name/quest-chains/quest-chains-xdai',
   },
   '0x5': {
     chainId: '0x5',
     name: 'Görli Testnet',
     label: 'Görli',
+    urlName: CHAIN_URL_MAPPINGS.goerli,
     symbol: 'ETH',
     explorer: 'https://goerli.etherscan.io',
     explorerLabel: 'Etherscan',
     rpc: `https://goerli.infura.io/v3/${INFURA_ID}`,
     image: EthereumImage.src,
-    subgraphName: 'dan13ram/quest-chains-goerli',
+    subgraphName: 'quest-chains/quest-chains-goerli',
     subgraphUrl:
-      'https://api.thegraph.com/subgraphs/name/dan13ram/quest-chains-goerli',
+      'https://api.thegraph.com/subgraphs/name/quest-chains/quest-chains-goerli',
   },
   '0x13881': {
     chainId: '0x13881',
     name: 'Mumbai Testnet',
     label: 'Mumbai',
+    urlName: CHAIN_URL_MAPPINGS.mumbai,
     symbol: 'MATIC',
     explorer: 'https://mumbai.polygonscan.com',
     explorerLabel: 'PolygonScan',
     rpc: 'https://rpc-mumbai.maticvigil.com',
     image: PolygonImage.src,
-    subgraphName: 'dan13ram/quest-chains-mumbai',
+    subgraphName: 'quest-chains/quest-chains-mumbai',
     subgraphUrl:
-      'https://api.thegraph.com/subgraphs/name/dan13ram/quest-chains-mumbai',
+      'https://api.thegraph.com/subgraphs/name/quest-chains/quest-chains-mumbai',
   },
 };
 
