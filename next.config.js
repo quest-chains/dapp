c8e9c11d2ab55ade36b77e752ee95b457f74c98d
vidvidvid <weetopol@gmail.com>
2022-12-22 18:14:14 +0100

    take care of the redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/chain/:slug*',
+        destination: '/:slug*',
+        permanent: true,
+      },
+    ];
+  },
 };
