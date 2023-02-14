a77bcffbe97db4626c9d7c20d24b0fb528359ea0
dan13ram <dan13ram@gmail.com>
2023-02-14 19:19:24 +0530

    update edit profile design

--- components/ProfileView/EditProfileModal.tsx
+++ components/ProfileView/EditProfileModal.tsx
@@ -14,19 +14,20 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
+  useDisclosure,
   VStack,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 import { useDropImage } from '@/hooks/useDropFiles';
-import { useInputText } from '@/hooks/useInputText';
 import { fetchWithHeaders } from '@/utils/fetchWithHeaders';
 import { uploadFiles } from '@/utils/metadata';
 import { ipfsUriToHttp } from '@/utils/uriHelpers';
 import { useWallet } from '@/web3';
 
+import { ConfirmationModal } from '../ConfirmationModal';
 import { SubmitButton } from '../SubmitButton';
 import { UploadImageForm } from '../UploadImageForm';
 
@@ -38,7 +39,7 @@ export const EditProfileModal: React.FC<{
 
   const { push } = useRouter();
 
-  const [usernameRef, setUsername] = useInputText();
+  const [username, setUsername] = useState(user?.username ?? '');
   const [usernameError, setUsernameError] = useState('');
 
   const uploadImageProps = useDropImage();
@@ -48,33 +49,50 @@ export const EditProfileModal: React.FC<{
 
   const [isLoading, setLoading] = useState(false);
 
+  const validateChanges = useCallback((): boolean => {
+    setUsernameError('');
+    setImageError('');
+    if (!username) {
+      setUsernameError('Username cannot be empty.');
+      return false;
+    }
+    if (username.length < 3) {
+      setUsernameError('Username must be between 3 and 30 characters long.');
+      return false;
+    }
+    if (!/^[A-Za-z0-9]+(?:.*[A-Za-z0-9]+)*$/.test(username)) {
+      setUsernameError('Username must start and end with a letter or number.');
+      return false;
+    }
+
+    if (!/^[A-Za-z0-9-_]+$/.test(username)) {
+      setUsernameError(
+        'Username can only contain letters, numbers, hyphens, and underscores.',
+      );
+      return false;
+    }
+
+    if (!/^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$/.test(username)) {
+      setUsernameError(
+        'Username cannot have consecutive or adjacent hyphens or underscores.',
+      );
+      return false;
+    }
+
+    const existingAvatarUri = user?.avatarUri;
+
+    if ((!existingAvatarUri || imageRemoved) && !imageFile) {
+      setImageError('Avatar image cannot be empty');
+      return false;
+    }
+
+    return true;
+  }, [user, username, imageFile, imageRemoved]);
+
   const handleUpdate = useCallback(async () => {
     let tid;
     try {
       setLoading(true);
-      const username = usernameRef.current;
-      if (!username) {
-        setUsernameError('Username cannot be empty.');
-        return;
-      }
-      if (username.length < 3) {
-        setUsernameError('Username must be at least 3 characters long.');
-        return;
-      }
-      if (!/^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$/.test(username)) {
-        setUsernameError('Username contains invalid characters.');
-        return;
-      }
-
-      const existingAvatarUri = user?.avatarUri;
-
-      if ((!existingAvatarUri || imageRemoved) && !imageFile) {
-        setImageError('Avatar image cannot be empty');
-        return;
-      }
-
-      setUsernameError('');
-      setImageError('');
 
       const toUpdate = { username, avatarUri: user?.avatarUri ?? '' };
 
@@ -94,7 +112,7 @@ export const EditProfileModal: React.FC<{
       if (res.status === 200) {
         setUser(await res.json());
         toast.dismiss(tid);
-        tid = toast.success('Profile successfully updated!');
+        tid = toast.success('Profile updated successfully!');
         onClose();
         push('/profile/' + username);
       } else {
@@ -108,11 +126,11 @@ export const EditProfileModal: React.FC<{
     } finally {
       setLoading(false);
     }
-  }, [usernameRef, imageFile, imageRemoved, user, setUser, push, onClose]);
+  }, [username, imageFile, user, setUser, push, onClose]);
 
   useEffect(() => {
-    if (isOpen && user) {
-      setUsername(user.username ?? '');
+    if (isOpen) {
+      setUsername(user?.username ?? '');
       onResetImage();
       setUsernameError('');
       setImageError('');
@@ -120,91 +138,156 @@ export const EditProfileModal: React.FC<{
     }
   }, [isOpen, user, setUsername, onResetImage]);
 
-  const [nameLength, setNameLength] = useState(usernameRef.current.length);
+  const {
+    isOpen: isConfirmationOpen,
+    onClose: onConfirmationClose,
+    onOpen: onConfirmationOpen,
+  } = useDisclosure();
 
-  if (!user) return null;
+  const [confirmationProps, setConfirmationProps] = useState<{
+    title: string;
+    content: string;
+    onSubmit: () => void;
+    cancelLabel?: string;
+    submitLabel?: string;
+  }>({ title: '', content: '', onSubmit: () => undefined });
+
+  const isChanged = useMemo(
+    () => imageRemoved || imageFile || username !== user?.username,
+    [user, username, imageFile, imageRemoved],
+  );
+
+  const onModalClose = useCallback(() => {
+    if (!isChanged) {
+      return onClose();
+    }
+
+    setConfirmationProps({
+      title: 'Discard Changes',
+      content:
+        'There are unsaved changes to your profile. Do you wish to discard them?',
+      onSubmit: () => {
+        onClose();
+        onConfirmationClose();
+      },
+      cancelLabel: 'Go back',
+      submitLabel: 'Discard',
+    });
+
+    onConfirmationOpen();
+  }, [isChanged, onClose, onConfirmationOpen, onConfirmationClose]);
+
+  const onSubmit = useCallback(() => {
+    if (!validateChanges()) {
+      return;
+    }
+
+    setConfirmationProps({
+      title: 'Save Changes',
+      content: 'Are you sure you want to save these changes to your profile?',
+      onSubmit: () => {
+        onConfirmationClose();
+        handleUpdate();
+      },
+      cancelLabel: 'Cancel',
+      submitLabel: 'Save',
+    });
+
+    onConfirmationOpen();
+  }, [onConfirmationOpen, onConfirmationClose, handleUpdate, validateChanges]);
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={!isLoading}>
-      <ModalOverlay />
-      <ModalContent maxW="48rem">
-        {!isLoading && <ModalCloseButton />}
-        <ModalHeader>Edit Profile</ModalHeader>
-        <ModalBody>
-          <form>
-            <VStack w="100%" align="stretch" mb={10} spacing={4}>
-              <FormControl w="full" isInvalid={!!usernameError} isRequired>
-                <Flex align="center" justify="space-between" w="100%">
-                  <FormLabel htmlFor="name">Username</FormLabel>
-                  <Text fontSize="sm">{nameLength} / 30</Text>
-                </Flex>
-                <Input
-                  color="white"
-                  isDisabled={isLoading}
-                  defaultValue={usernameRef.current}
-                  bg="#0F172A"
-                  id="name"
-                  maxLength={30}
-                  onChange={e => {
-                    setUsername(e.target.value);
-                    setNameLength(e.target.value.length);
-                  }}
-                  placeholder="username"
-                />
-                {usernameError ? (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onModalClose}
+        closeOnOverlayClick={!isLoading}
+      >
+        <ModalOverlay />
+        <ModalContent maxW="30rem">
+          {!isLoading && <ModalCloseButton />}
+          <ModalHeader>Edit Profile</ModalHeader>
+          <ModalBody>
+            <form>
+              <VStack w="100%" align="stretch" mb={10} spacing={8}>
+                <FormControl w="full" isInvalid={!!usernameError}>
+                  <Flex align="center" justify="space-between" w="100%">
+                    <FormLabel htmlFor="name">Username</FormLabel>
+                    <Text fontSize="sm">{username.length} / 30</Text>
+                  </Flex>
+                  <Input
+                    color="white"
+                    isDisabled={isLoading}
+                    value={username}
+                    pattern="[a-zA-z0-9_-]"
+                    bg="#0F172A"
+                    id="name"
+                    maxLength={30}
+                    onChange={e => setUsername(e.target.value)}
+                    placeholder="username"
+                  />
                   <FormErrorMessage>{usernameError}</FormErrorMessage>
-                ) : (
                   <FormHelperText>
-                    Enter a username that will be visible across the Quest
-                    Chains platform. It must be atleast 3 characters long and
-                    contain only _ or -. It must also start and end with an
-                    alphanumeric character.
+                    Choose a username that represents you across the platform.
+                    Your username must be 3-30 characters long, start and end
+                    with a letter or number, and only contain letters, numbers,
+                    hyphens, and underscores. No consecutive or adjacent hyphens
+                    or underscores allowed.
                   </FormHelperText>
-                )}
-              </FormControl>
+                </FormControl>
 
-              <UploadImageForm
-                {...uploadImageProps}
-                label="Avatar"
-                formControlProps={{
-                  w: '100%',
-                  h: '16rem',
-                  position: 'relative',
-                  isRequired: true,
-                }}
-                imageProps={{
-                  h: '16rem',
-                  w: '16rem',
-                }}
-                dropzoneProps={{
-                  ...uploadImageProps.dropzoneProps,
-                  width: '100%',
-                  height: '16rem',
-                }}
-                defaultImageUri={ipfsUriToHttp(user.avatarUri)}
-                isDisabled={isLoading}
-                errorMessage={imageError}
-                helperText="Upload an image as your avatar that will be visible across the Quest Chains platform"
-                onResetDefaultImage={() => setImageRemoved(true)}
-              />
-            </VStack>
-          </form>
-        </ModalBody>
-        <ModalFooter alignItems="baseline">
-          <Button
-            variant="ghost"
-            mr={3}
-            isDisabled={isLoading}
-            onClick={onClose}
-            borderRadius="full"
-          >
-            Close
-          </Button>
-          <SubmitButton mt={4} isLoading={isLoading} onClick={handleUpdate}>
-            Submit
-          </SubmitButton>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+                <UploadImageForm
+                  {...uploadImageProps}
+                  label="Avatar"
+                  formControlProps={{
+                    w: '100%',
+                    h: '16rem',
+                    position: 'relative',
+                  }}
+                  imageProps={{
+                    h: '16rem',
+                    w: '16rem',
+                  }}
+                  dropzoneProps={{
+                    ...uploadImageProps.dropzoneProps,
+                    width: '100%',
+                    height: '16rem',
+                  }}
+                  defaultImageUri={ipfsUriToHttp(user?.avatarUri)}
+                  isDisabled={isLoading}
+                  errorMessage={imageError}
+                  helperText="Personalize your profile by uploading an image that represents you across the platform. This image will be visible to other users."
+                  onResetDefaultImage={() => setImageRemoved(true)}
+                />
+              </VStack>
+            </form>
+          </ModalBody>
+          <ModalFooter alignItems="baseline" mt={6}>
+            <Button
+              variant="ghost"
+              mr={3}
+              isDisabled={isLoading}
+              onClick={onModalClose}
+              borderRadius="full"
+            >
+              Close
+            </Button>
+            <SubmitButton
+              mt={4}
+              isLoading={isLoading}
+              onClick={onSubmit}
+              isDisabled={!isChanged}
+            >
+              Submit
+            </SubmitButton>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <ConfirmationModal
+        isOpen={isConfirmationOpen}
+        onClose={onConfirmationClose}
+        {...confirmationProps}
+      />
+    </>
   );
 };
