28884bd500e509a453dec5d13303067f9583c445
dan13ram <dan13ram@gmail.com>
2023-03-04 13:58:44 +0530

    removed landing page

--- next.config.js
+++ next.config.js
@@ -3,48 +3,8 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/chain/:slug*',
-        destination: '/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/chain/0x13881/:slug*',
-        destination: '/mumbai/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/chain/0x89/:slug*',
-        destination: '/polygon/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/chain/0x64/:slug*',
-        destination: '/gnosis/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/chain/0x5/:slug*',
-        destination: '/goerli/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/0x13881/:slug*',
-        destination: '/mumbai/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/0x89/:slug*',
-        destination: '/polygon/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/0x64/:slug*',
-        destination: '/gnosis/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/0x5/:slug*',
-        destination: '/goerli/:slug*',
+        source: '/',
+        destination: '/explore',
         permanent: true,
       },
     ];
