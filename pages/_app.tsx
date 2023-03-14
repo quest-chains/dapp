dc29b8ece4e0b7a224032d04e398854a37367e61
dan13ram <dan13ram@gmail.com>
2023-03-14 16:12:13 +0530

    fixed links in header & footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,40 +34,38 @@ const ForceDarkMode: React.FC<PropsWithChildren> = ({ children }) => {
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <>
-      <div className="background">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <Script
-        type="text/javascript"
-        defer
-        data-domain="questchains.xyz"
-        data-api="/jjmahtdkrp/api/event"
-        src="/jjmahtdkrp/js/script.js"
-      />
-      <ChakraProvider resetCSS theme={theme}>
-        <ForceDarkMode>
-          <Global styles={globalStyles} />
-          <WalletProvider>
-            <AppLayout>
-              <Component {...pageProps} />
-            </AppLayout>
-          </WalletProvider>
-          <Toaster
-            position="bottom-center"
-            toastOptions={{
-              style: {
-                borderRadius: '1rem',
-                maxWidth: '40rem',
-                marginBottom: '2rem',
-              },
-            }}
-          />
-        </ForceDarkMode>
-      </ChakraProvider>
-    </>
+    <ChakraProvider resetCSS theme={theme}>
+      <ForceDarkMode>
+        <div className="background">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <Script
+          type="text/javascript"
+          defer
+          data-domain="questchains.xyz"
+          data-api="/jjmahtdkrp/api/event"
+          src="/jjmahtdkrp/js/script.js"
+        />
+        <Global styles={globalStyles} />
+        <WalletProvider>
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        </WalletProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: {
+              borderRadius: '1rem',
+              maxWidth: '40rem',
+              marginBottom: '2rem',
+            },
+          }}
+        />
+      </ForceDarkMode>
+    </ChakraProvider>
   );
 };
 
