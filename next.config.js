902ef4c1b63db6782065ab93d1bcd70cbe592997
Bitcoinera <anagjordano@gmail.com>
2023-11-09 10:20:11 +0100

    fix web3 name sdk build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  transpilePackages: ['@web3-name-sdk/core'],
   async redirects() {
     return [
       {
